82017f16c51782daa2d091a300aad2041b3002ec
Rogier Nitschelm <r.nitschelm@gmail.com>
2023-03-05 10:22:43 +0100

    company expose fields

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,6 +6,7 @@ class Company < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :location, presence: true, length: { minimum: 5, maximum: 50 }
   validates :website_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
+  validates :description, presence: true, length: { minimum: 20, maximum: 5_000 }
 
   has_one_attached :logo
 end
